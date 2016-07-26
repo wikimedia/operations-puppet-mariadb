6df39e880d113553cce626c4f89608224e762171
Jcrespo <jcrespo@wikimedia.org>
2016-07-26 12:53:30 +0200

    Ignore trace filesystems on disk check
    This is only a workaround until this is deprecated in favour of
    the common check, with newly added parameters
    
    Change-Id: I79e7c7dacaa704cbe96154fa4da000b0294fef61

--- manifests/monitor_disk.pp
+++ manifests/monitor_disk.pp
@@ -1,5 +1,7 @@
 # TODO: Revisit the is_critical part. We probably want pages for DB problems for
 # at least a group of people
+# TODO: Drop this check and use the common check_disk, with the newly added
+# parameters
 class mariadb::monitor_disk(
     $is_critical   = true,
     $contact_group = 'dba',
@@ -8,7 +10,8 @@ class mariadb::monitor_disk(
     ) {
     nrpe::monitor_service { 'mariadb_disk_space':
         description   => 'MariaDB disk space',
-        nrpe_command  => "/usr/lib/nagios/plugins/check_disk -w ${pct_warning}% -c ${pct_critical}% -l -e",
+        nrpe_command  => "/usr/lib/nagios/plugins/check_disk \
+-w ${pct_warning}% -c ${pct_critical}% -l -e --exclude-type=tracefs",
         critical      => $is_critical,
         contact_group => $contact_group,
     }
