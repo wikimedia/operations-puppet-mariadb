29056afbcec4f18641930b5fa37ff97c997e7625
Jcrespo <jcrespo@wikimedia.org>
2016-10-11 12:48:45 +0200

    mariadb:Create a systemd unit & init.d script for new package
    The systemd unit has been on purpose not included on the package
    so it can be handled by puppet. The config changes should also help
    securize running mysqld.
    
    The only configurable variables are for now @basedir and
    @open_files_limit (that last one only on systemd).
    
    Until we upgrade to 10.1 or 5.7 and get rid of trustys, we will
    continue using init.d due to the lack of support on the older
    (for us, current) versions for systemd.
    
    Bug: T147305
    Change-Id: I23cee27c8e86c94dc830cde799a50955f9513c9b

---
+++ manifests/service.pp
@@ -0,0 +1,33 @@
+# Make /etc/init/mysql managed by puppet. This allows us to make quick
+# changes to harden the wrapper without rebuilding the custom wmf-mariabd10
+# package
+# Once all trusty dbs are gone, we can hopefully discard init.d in favour
+# of a custom systemd service unit
+class mariadb::service (
+    $package = 'wmf-mariadb10',
+    ) {
+
+    $basedir = "/opt/${package}"
+    file { "${basedir}/service":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        content => template('mariadb.server.erb'),
+        require => Package["${package}"],
+    }
+
+    file { '/etc/init.d/mysql':
+        ensure  => 'link',
+        target  => "${basedir}/service",
+        require => "${basedir}/service",
+    }
+
+    file { '/etc/init.d/mariadb':
+        ensure  => 'link',
+        target  => "${basedir}/service",
+        require => "${basedir}/service",
+    }
+
+    # MySQL DOES NOT START BY DEFAULT- do not register it automatically
+}
